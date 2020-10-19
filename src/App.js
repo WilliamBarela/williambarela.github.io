2abcc6091d4a5fa422bfaab2d6e7207aad67101f
William Barela <william.barela.dev@gmail.com>
2020-10-19 02:55:04 -0500

    build basic components for homepage to bring site back to state while in vanila js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import logo from './img/wb-dark.svg';
+import Header from './components/header';
+import Main from './components/main';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
 
