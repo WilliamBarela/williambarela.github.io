591fa3d00d4695a80833112458195f3cf2c1fde4
William Barela <william.barela.dev@gmail.com>
2020-10-19 01:19:55 -0500

    Updates

---
+++ precache-manifest.6e228279894a3da7c59812c174a70e55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2c4217f2f2f0197ea9fab6844187d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2bcbd5cbb8d37c2e94f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2728bd46229110998ed3",
+    "url": "/static/js/2.4f2ddc90.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.4f2ddc90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2bcbd5cbb8d37c2e94f",
+    "url": "/static/js/main.dfeb2bf8.chunk.js"
+  },
+  {
+    "revision": "c3348f535beb133c5ee5",
+    "url": "/static/js/runtime-main.0c530d9f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-import Display from './Display.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-  const display = new Display;
-  display.addNavListener;
-  display.addHero;
-});
