ea0cbba4d5ba9a5a2af86226e04e3bfd3a2adbec
William Barela <william.barela.dev@gmail.com>
2020-10-25 09:48:21 -0500

    refactor header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,20 +1,9 @@
 import React, { Component } from 'react';
-import wbLogo from '../../img/wb-dark.svg';
-import hamburger from '../../img/hamburger-light.svg';
-import close from '../../img/close.svg';
 
+import Logo from '../icons/Logo';
+import PrimaryNav from '../navs/PrimaryNav';
+import PrimaryNavSwitch from '../navs/PrimaryNavSwitch';
 
-const MenuIcon = () => {
-  return(
-    <img src={hamburger} alt="Main Menu" id="hamburger" />
-  )
-};
-
-const CloseIcon = () => {
-  return(
-    <img src={close} alt="Close" id="close" />
-  )
-};
 
 
 class Header extends Component {
@@ -54,21 +43,15 @@ class Header extends Component {
   render() {
     return (
       <header>
-      <nav id="top-nav">
-          <a href="/">
-            <img src={wbLogo} alt="William Barela Logo" id="wb-logo" />
-          </a>
-          <div id="primary-navegation" className={this.state.primaryNav.style}>
-            <ul className="primary-navegation-content">
-              <li><a href="">about</a></li>
-              <li><a href="">projects</a></li>
-              <li><a href="">skills</a></li>
-              <li><a href="/devblog">blog</a></li>
-            </ul>
-          </div>
-          <a href="#" onClick={this.handleMenuClick}>
-            { this.state.menu.display ? <MenuIcon /> : <CloseIcon />}
-          </a>
+        <nav id="top-nav">
+          <Logo />
+          <PrimaryNav
+            primaryNav={this.state.primaryNav}
+          />
+          <PrimaryNavSwitch
+            handleMenuClick={this.handleMenuClick}
+            menu={this.state.menu}
+          />
         </nav>
       </header>
     )
